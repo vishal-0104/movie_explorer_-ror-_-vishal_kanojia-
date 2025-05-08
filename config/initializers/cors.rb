8a3121c3c4688969c71ab5197af97a145b284d6d
vishal2110991544 <vishal1544.be21@chitkara.edu.in>
2025-05-08 14:12:34 +0530

    [vishal]Add: cors for frontend and production database url

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000', 'http://localhost:3001' # Frontend URLs
+    origins '*' 
     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options]
   end
 end
